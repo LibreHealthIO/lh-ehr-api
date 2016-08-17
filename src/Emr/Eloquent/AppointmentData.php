7f5abb05b12e1715f45ac2e31566879fb2d700a9
Leo24 <leo2410@yandex.ru>
2016-08-17 18:28:30 +0300

    Start implement List appointments by patient

--- src/Emr/Eloquent/AppointmentData.php
+++ src/Emr/Eloquent/AppointmentData.php
@@ -27,7 +27,7 @@ class AppointmentData extends Model implements AppointmentInterface
 
     public function getStartTime()
     {
-        return $this->pc_startTime;
+        return $this->getPcEventDate() . ' ' . $this->pc_startTime;
     }
 
     public function setStartTime( $startTime )
@@ -38,12 +38,83 @@ class AppointmentData extends Model implements AppointmentInterface
 
     public function getEndTime()
     {
-        return $this->pc_endTime;
+        return $this->getPcEventDate() . ' ' . $this->pc_endTime;
     }
 
     public function setEndTime( $endTime )
     {
         $this->pc_endTime = $endTime;
         return $this;
+    }
+
+    public function getPcEventDate()
+    {
+        return $this->pc_eventDate;
+    }
+    public function setPcEventDate($pcEventDate)
+    {
+        $this->pc_eventDate = $pcEventDate;
+        return $this;
+    }
+
+    public function getPcApptStatus()
+    {
+        return $this->decodeStatus($this->pc_apptstatus);
+    }
+    public function setPcApptStatus($pcApptstatus)
+    {
+        $this->pc_apptstatus = $pcApptstatus;
+        return $this;
+    }
+
+    public function getPcDuration()
+    {
+        return $this->pc_duration;
+    }
+    public function setPcDuration($pcDuration)
+    {
+        $this->pc_duration = $pcDuration;
+        return $this;
+    }
+
+    public function getPcTime()
+    {
+        return $this->pc_time;
+    }
+    public function setPcTime($pcTime)
+    {
+        $this->pc_time = $pcTime;
+        return $this;
+    }
+
+    private function decodeStatus($status)
+    {
+        switch($status) {
+            case '+': $decodeStatus = 'Chart pulled';
+                break;
+            case 'x': $decodeStatus = 'Canceled';
+                break;
+            case '?': $decodeStatus = 'No show';
+                break;
+            case '@': $decodeStatus = 'Arrived';
+                break;
+            case '~': $decodeStatus = 'Arrived late';
+                break;
+            case '!': $decodeStatus = 'Left w/o visit';
+                break;
+            case '#': $decodeStatus = 'Ins/fin issue';
+                break;
+            case '<': $decodeStatus = 'In exam room';
+                break;
+            case '>': $decodeStatus = 'Checked out';
+                break;
+            case '$': $decodeStatus = 'Coding done';
+                break;
+            case '%': $decodeStatus = 'Canceled &lt; 24h';
+                break;
+            default: $decodeStatus = '';
+        }
+
+        return $decodeStatus;
     }
 }
--- src/Emr/Repositories/AppointmentRepository.php
+++ src/Emr/Repositories/AppointmentRepository.php
@@ -31,19 +31,151 @@ class AppointmentRepository extends AbstractRepository implements AppointmentRep
 
     public function create( AppointmentInterface $appointmentInterface )
     {
-        echo '<pre>'; var_dump('zzz'); echo '</pre>'; exit;
         if ( is_a( $appointmentInterface, $this->model() ) ) {
-
-
             $appointmentInterface->save();
             $appointmentInterface = $this->get( $appointmentInterface->id );
-
         }
 
         return $appointmentInterface;
+    }
+
+    public function getSlots($startDate )
+    {
+        $busySlots = DB::table('openemr_postcalendar_events')
+            ->where('pc_eventDate', '=', $startDate)
+            ->get();
+
+        $param = array(
+            'scheduleStart' => 8,
+            'scheduleEnd' => 17,
+            'calendarInterval' => 15,
+            );
+
+        $allSlots = $this->addFreeSlots($busySlots, $param);
+        $slotFormat = $this->slotFormat($allSlots, $param);
+
+        return $slotFormat;
+    }
+
+    private function slotFormat($allSlots, $param)
+    {
+
+        $slotFormat = array();
+        foreach ($allSlots as $key => $slot) {
+
+            $slotFormat[$key]['startTime'] = date("H:i:s", mktime($slot['hour'], $slot['minute'], 0, 0, 0, 0));
+            $slotFormat[$key]['endTime'] = date("H:i:s", mktime($slot['hour'], $slot['minute'] + $param['calendarInterval'], 0, 0, 0, 0));
+            $slotFormat[$key]['status'] = $slot['status'];
+        }
+        return $slotFormat;
 
     }
 
+
+    public function addFreeSlots($busy_slots, $param)
+    {
+        if($busy_slots) {
+            $i = 0;
+            foreach($busy_slots as $slot) {
+                $busyInterval[$i]['startTime'] = $slot->pc_startTime;
+                $busyInterval[$i]['endTime'] = $slot->pc_endTime;
+                $i++;
+            }
+            $scheduleStart = $param['scheduleStart'];
+            $scheduleEnd = $param['scheduleEnd'];
+            $calendarInterval = $param['calendarInterval'];
+
+            // $times is an array of associative arrays, where each sub-array
+            // has keys 'hour', 'minute' and 'mer'.
+            //
+            $allSlots = array();
+
+            // For each hour in the schedule...
+            //
+            for($blockNum = $scheduleStart; $blockNum <= $scheduleEnd; $blockNum++){
+
+                // $minute is an array of time slot strings within this hour.
+                $minute = array('00');
+
+                for($minutes = $calendarInterval; $minutes <= 60; $minutes += $calendarInterval) {
+                    if($minutes <= '9'){
+                        $under_ten = "0" . $minutes;
+                        array_push($minute, "$under_ten");
+                    }
+                    else if($minutes >= '60') {
+                        break;
+                    }
+                    else {
+                        array_push($minute, "$minutes");
+                    }
+                }
+
+                foreach($minute as $m ){
+                    array_push($allSlots, array("hour"=>$blockNum, "minute"=>$m, "status" => "free"));
+                }
+            }
+
+            foreach ($allSlots as $key => $slot) {
+
+                foreach($busyInterval as $busy) {
+                    $arStart = explode(':',$busy['startTime']);
+                    if($arStart[0] < $scheduleStart){
+                        $arStart[0] = (int)$arStart[0] + 12;
+                    }
+                    if($slot['hour'] != $arStart[0]){
+                        continue;
+                    }
+                    if($slot['minute'] != $arStart[1]){
+                        continue;
+                    }
+                    $arEnd = explode(':',$busy['endTime']);
+                    if($arEnd[0] < $scheduleStart){
+                        $arEnd[0] = (int)$arEnd[0] + 12;
+                    }
+
+                    $busyKey = $this->busySlotKey($allSlots, $key, $arEnd);
+
+                    foreach ($busyKey as $key){
+                        $allSlots[$key]['status'] = 'busy';
+                    }
+                }
+            }
+
+            return $allSlots;
+        }
+    }
+
+    private function busySlotKey(&$allSlots, $keyStart, array $endTime)
+    {
+
+        $busyKey = array();
+        for ($i = $keyStart; $i <= count($allSlots); $i++) {
+            if($allSlots[$i]['hour'] > $endTime[0]) {
+                break;
+            }
+            if($allSlots[$i]['hour'] == $endTime[0] && $allSlots[$i]['minute'] >= $endTime[1]) {
+                break;
+            }
+            $allSlots[$i]['status'] = 'busy';
+        }
+        return $busyKey;
+
+    }
+
+    public function getAppointmentsByParam($patientID, $data)
+    {
+        if(isset($data['date_le']) && isset($data['date_ge'])) {
+            $appointments = Appointment::whereRaw('pc_pid = ? AND pc_eventDate > ? AND pc_eventDate < ?', array($patientID, $data['date_le'], $data['date_ge']))
+                ->get();
+        }
+
+        return $appointments;
+    }
+
+    public function getUnavailableSlots()
+    {
+
+    }
 
     public function update( $id, array $data )
     {
