ebb24111c74890da822a883670bdaa3bd1e974d2
Leo24 <leo2410@yandex.ru>
2016-10-04 12:08:31 +0300

    Add functionality to update Appointment

--- src/Emr/Repositories/AppointmentRepository.php
+++ src/Emr/Repositories/AppointmentRepository.php
@@ -40,10 +40,48 @@ class AppointmentRepository extends AbstractRepository implements AppointmentRep
         return $appointmentInterface;
     }
 
-    public function update($id, $status)
+    public function update($id, $data)
     {
-        $appointmentInterface = Appointment::connection($this->connection)->find($id);
-        $appointmentInterface->setPcApptStatus($status);
+
+        $appointment = new Appointment();
+        $appointment->setConnectionName($this->connection);
+        $appointmentInterface = $appointment->find($id);
+        foreach ($data as $k => $ln) {
+            if ($k == 'status') {
+                $appointmentInterface->setPcApptStatus($ln);
+            }
+            if ($k == 'description') {
+                $appointmentInterface->setDescription($ln);
+            }
+            if ($k == 'start') {
+                $appointmentInterface->setStartTime($ln);
+            }
+            if ($k == 'end') {
+                $appointmentInterface->setEndTime($ln);
+            }
+            if ($k == 'extension') {
+                $extensions = $ln[0]['extension'];
+                $location = [];
+                foreach ($extensions as $extension) {
+                    $url = $extension['url'];
+                    if ($url =="#portal-uri") {
+                        $location['portalUri'] = $extension['valueString'];
+                    }
+                    if ($url =="#room-key") {
+                        $location['roomKey'] = $extension['valueString'];
+                    }
+                    if ($url =="#pin") {
+                        $location['pin'] = $extension['valueString'];
+                    }
+                }
+                $appointmentInterface->setLocation(json_encode($location, true));
+            }
+        }
+
+
+
+
+
         $appointmentInterface->save();
 
         return $appointmentInterface;
