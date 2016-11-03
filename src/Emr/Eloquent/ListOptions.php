fcf80dcf2b619bf4152a6164aa6830fc4028f546
Ken Chapple <ken.chapple@gmail.com>
2016-11-03 17:17:24 -0400

    added list options (for counties)

---
+++ src/Emr/Eloquent/ListOptions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LibreEHR\Core\Emr\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ListOptions extends Model
+{
+    protected $table = 'list_options';
+
+    protected $connection = 'auth';
+
+}
---
+++ src/Emr/Repositories/ListOptionsRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace LibreEHR\Core\Emr\Repositories;
+
+use LibreEHR\FHIR\Models\Option;
+
+class ListOptionsRepository extends AbstractRepository
+{
+    public function model()
+    {
+        return '\LibreEHR\Core\Emr\Eloquent\ListOptions';
+    }
+
+    public function find()
+    {
+        return parent::find();
+    }
+
+    public function fetchOptionsByListId( $listId )
+    {
+        $model = $this->makeModel();
+        $results = $model->where( 'list_id', '=', $listId )->get();
+        return $this->processResult( $results );
+    }
+
+    public function processResult( $results )
+    {
+        $options = array();
+        foreach ( $results as $r ) {
+
+            $option = new Option( $r['title'], $r['option_id'] );
+            $options[]= $option;
+        }
+
+        return $options;
+    }
+}
